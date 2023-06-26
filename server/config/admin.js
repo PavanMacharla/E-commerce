ac7bf44846d4064472650d0fd285c83b1d1931a6
PavanMacharla <macharlapavan440@gmail.com>
2023-06-26 08:42:51 +0530

    dfs

--- server/config/admin.js
+++ server/config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: 'GFf4qMIBvDK3amvGRdj7Mw==',
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: 'FHHfAfGyZ9vrfQT4WHnV8Q==',
   },
 });
--- server/config/server.js
+++ server/config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['yHOzJJ1i7F2hSXQNM93A7A==','vein/64aN0uDqkkD153TlA==','bkvCqq4ZSZ8p+UaYO11s0A==','0YAR6oj6k7UH6Q06pW52Hw=='],
   },
 });
